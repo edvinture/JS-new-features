a520df8a7f6661365b2177354bef243b4c25bd71
IvanSerov <VanSer1989@gmail.com>
2021-09-01 12:32:16 +0200

    интеполяция

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const typeGoods = 'food'
+
+//console.log('https://market.com/' + typeGoods );
+
+//console.log('https://market.com/' + typeGoods + '|' + '10');
+
+console.log('https://market.com/${typeGoods}');
+
+/*
+задание
+
+Вывести в консоль с помощью интеполяции адрес 
+https://market.com/games/strategy/10
+сохранить в github. коммит - интеполяция 
+
+/*
+
+
