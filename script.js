20e9b9310ecb0a35d067c1d5dd692ca12507edfa
IvanSerov <VanSer1989@gmail.com>
2021-09-17 07:49:23 +0200

    fichi 3

--- script.js
+++ script.js
@@ -1,268 +1,252 @@
-"use strict";
-
-const typeGoods = 'food'
-
-//console.log('https://market.com/' + typeGoods );
-
-//console.log('https://market.com/' + typeGoods + '|' + '10');
-
-console.log(`https://market.com/${typeGoods}`);
-
-/*
-задание
-
-Вывести в консоль с помощью интеполяции адрес 
-https://market.com/games/strategy/10
-сохранить в github. коммит - интеполяция 
-
-*/
-
-"use strict";
-
-let number = 10;
-
-function showMessage(text) {
-    console.log(text);
-    //let number = 20;
-    console.log(number)
-}
-
-showMessage("Hello!!!");
-console.log(number);
-
-const server = function() {
-    console.log('server start...');
-}
-
-server();
-
-const calc = (x, y) => x+y;
-// const calc = (x, y) => {
-//      return x+y    
-//};
-
-//const calc = x => x+10;
-
-console.log(calc(10,5));
-
-
-
-
 "use strict"
 
-function server(){
-    setTimeout(function(){
-        console.log(1);
-    },1000)
-}
-function foo (){
-            console.log(2);
-    }
+//svoystva-aksessori get set
 
+const person = {
+    name: "John",
+    age: 28,
 
-server();
-foo();
-
-function serverNew(host, callback){
-    console.log(`Server ${host} is starting...`);
-    callback();
-}
-
-//serverNew('MyServer', function (){
-//  console.log('connect success!');
-//   }); 
-
-function done (){
-    console.log('connect success!');
-}
-
-serverNew('MyServer', done);
-
-//
-
-"use strict";
-
-//const obj = new Object (); //odin iz variantov sozdaniya objecta
-
-//samiy 4astiy variant sozdaniya objekta 
-const option = {
-    name: 'test',
-    width: '640',
-    height: '480',
-    colors: {
-        border: 'black',
-        bg: 'red'
-    }
-};
-
-console.log(option.name);
-console.log(option['name']); // rezhe ispolzuemiy variant notacii
-
-//delete option.name; //udaleniye kakogo-libo svoystva iz objecta
-
-console.log(option);
-
-//perebrat' vse elementi objecta
-for (let key in option){
-    console.log(`Свойство ${key} имеет значение ${option[key]}`);
-}
-
-//perebrat' vse elementi objecta
-let counter = 0;
-for (let key in option) {
-    if (typeof(option[key]) === 'object') {
-        for (let i in option [key]) {
-            console.log(`свойство ${i} имеет значение ${option[key][i]}`)
-            counter++;
-        }
-    }else{
-        console.log(`Свойство ${key} имеет значение ${option[key]}`);
-        counter++;
-    }
-}
-
-//koli4estvo svoystv v objecte
-console.log(counter);
-
-console.log(Object.keys(option));
-console.log(Object.keys(option).length);
-
-//dobavim povedeniye dlua objecta
-const option2 = {
-    name: 'test',
-    width: '640',
-    height: '480',
-    colors: {
-        border: 'black',
-        bg: 'red'
+    get userAge() {
+        return this.age;
     },
-    makeTest: function() {  //metod objecta
-        console.log("Yes!");
+    set userAge(num){
+        this.age = num;
     }
 };
 
-option2.makeTest();
+//svoystva! poetomu obrawaemsua bez skobok, imenno kak k svoystva, a ne k metodu
+console.log(person.userAge);
+console.log(person.userAge = 30);
+console.log(person.userAge);
 
-//destructirizaciya objecta(vitaskivaem nuznie nam svoystva)
+//inkapulyacia
 
-const {border,bg} = option2.colors;
-console.log(border);
+function User (name, age){
+    this.name = name;
+    this.age = age;
+
+    this.say = function (){
+        console.log(`Имя пользователя : ${this.name}, возраст ${this.age}`);
+    };
+}
+
+const john = new User ('John', 22);
+john.age = 30;
+console.log(john.name);
+console.log(john.age);
+john.say();
+
+//c inkapsylyaciyey
+
+function User2 (name, age) {
+    this.name = name;
+    let userAge = age;
+
+    this.say = function(){
+        console.log(`Имя пользователя: ${this.name}, возраст ${userAge}`);
+    };
+}
+
+const ann = new User2 ('Ann', 22);
+
+console.log(ann.name);
+console.log(ann.userAge);
+ann.say();
+
+////////////////////////////////////////////////
 
 
-//Aaaa
+function User3 (name, age) {
+    this.name = name;
+    let userAge = age;
+
+    this.say = function () {
+        console.log(`Имя пользователя: ${this.name}, возраст ${userAge}`);
+    };
+
+    this.getAge = function() {
+        return userAge;
+    }
+    this.setAge = function(age){
+        if(typeof age === 'number' && age > 0 && age <100) {
+            userAge = age;
+        }else{
+            console.log('недопустимое значение!')
+        }
+    }
+}
+
+const kirill = new User3('Kirill', 25);
+
+console.log(kirill.name);
+console.log(kirill.userAge);
+console.log(kirill.getAge());
+kirill.setAge(33);
+kirill.setAge(333);
+kirill.setAge('fwefwe');
+kirill.say();
+
+/////////////////////////////
+//inkapsylyaciya . klass
+
+
+class Author {
+    constructor (name,age) {
+        this.name = name;
+        this._age = age;
+    }
+
+    #surname = 'Maal'; //private pole
+
+    say = () => {
+        console.log(`Имя пользователя: ${this.name}, ${this.#surname} возраст ${this._age}`);
+    }
+
+    get age(){
+        return this._age;
+    }
+    set age(age) {
+        if (typeof age === 'number' && age > 0 && age <100){
+            this._age = age;
+        }else{
+            console.log('недопустимое значение !')
+
+        }
+    }
+}
+
+const ivan = new Author ('Ivan', 25);
+
+console.log(ivan.name);
+ivan.age = 99;
+console.log(ivan.age);
+
+ivan.say();
+
+//////////////222222222222222222222
 
 "use strict";
 
-const arr = [1,2,5,8,9];
+const num = 1;
 
-console.log(arr);
+(function(){
+    let num = 2;
+    console.log(num);
+    console.log(num + 1 );
+}());
 
-//udalit' posledniy element iz massiva
-arr.pop();
-console.log(arr);
+console.log(num);
 
-arr.push(10);  //dobavit' element v konec massiva
-console.log(arr);
+const user = (function(){
+    const privat = function () {
+        console.log('I am privat');
+    };
 
-//perebrat' elementi
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-}
+    return {
+        sayHello: privat // exportiruem naruzu local svoystvo
+    }
+}());
 
-for (let value of arr ) { //ne rabotaet s objecta
-    console.log(value);
-}
+user.sayHello();
 
-//dlina massiva ne vsegda dlina) posledniy index, plus 1
-const arr2 = [1,2,5,8,9];
-//arr2[99] = 0;  //tak delat' nelzua)
-console.log(arr2.length);
-console.log(arr2);
 
-//metod massivov
-//4asto ispolzuyetsya dlua perebora. Nedostatok v otli4ii ot obi4nih ciklov - nelzua ostanovit'
-arr2.forEach (function (item, i, arr2) {  //call-back funcia
-    console.log(`$ {i}: ${item} внутри массива ${arr2}`);
+//rab variant 3 error
+console.log("Запрос данных...");
+const req = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        console.log("подготовка данных...");
 
+        const product = {
+            name: 'phone',
+            price: 300
+        };
+
+        resolve(product);
+    },2000);
 });
 
-//sroku v massiv. naprimer bolwoy spisol tovarov 4erez zapyatuyu - v massiv
-const str = prompt ("","");
-const products = str.split(",");
-console.log(products);
-products.sort();  //vsegda sortiruyut kak stroki
-console.log(products);
-//obratnaya operaciya
-console.log(products.join("; "));
-
-//funkcia dlua sortirovki 4isle. peredayetsya v call-back
-//opicanie http://algolist.ru/sort/quick_sort.php
-products.sort(compareNum);
-function compareNum(a,b){
-    return a-b;
+req.then((product)=>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=> {
+            product.status = 'order';
+            //resolve(product);
+            reject();
+        }, 2000);
+    });
+}).then(data=>{
+    data.modify = 'true';
+    return data;
+}).then(data=>{
+    console.log(data);
+}).catch(()=>{
+    console.error('ERROR!')
 }
-console.log(products);
+);
 
-//metodi ne rabotayut dlua psevdomassivov
+//rabo4iy variant 4 finanly
+console.log ("запрос данных...");
+const req = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        console.log("Подготовка данных...");
 
-//333
+        const product = {
+            name: 'phone',
+            price: 300
+        };
+
+        resolve(product);
+    },2000);
+});
+
+req.then((product)=>{
+    return new Promise((resolve, reject)=> {
+        setTimeout(()=>{
+            product.status = 'order';
+            resolve(product);
+            reject();
+        },2000);
+    });
+}).then(data=>{
+    data.modify = 'true';
+    return data;
+}).then (data=> {
+    console.log(data);
+}).catch(()=>{
+    console.error('ERROR!')
+}).finally(()=>{
+    console.log("Все действия выполнены !");
+});
+
+//------------------------------------------------
+const test  = time => {
+    return new Promise ( resolve => {
+        setTimeout (()=> resolve(), time);
+    });
+};
+
+test(1000).then(() => console.log('1000 ms'));
+test(2000).then(() => console.log('2000 ms'));
+
+Promise.all([test(4000),  test (1000)]).then (()=>{
+    console.log('All');
+});
+
+////////////////////FETCH API  
 
 "use strict";
 
-//pereda4a po zna4eniyu (primitivnie tipi dannih)
-let a = 5;
-    b = a;
+//https://jsonplaceholder.typicode.com/  API
 
-b = b + 5;
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+.then(response => response.json ())
+.then(json => console.log (json));
 
-console.log(b);
-console.log(a);
-
-const obj = {
-    a: 5,
-    b: 1
-}
-
-const copy = obj;
-
-copy.a = 10;
-
-console.log(obj);
-console.log(copy);
-
-const newObj = Object.assign ({}, obj);
-newObg.a = 15;
-console.log (newObj);
-
-const oldArray = ['a', 'x' 'y'];
-const newArray = oldArray.slice();
-
-newArray[0] = 5;
-console.log(oldArray);
-console.log(newArray);
-
-const video = [ 'youtube', 'vimeo', 'facebook'],
-      blogs = ['wordpress', 'twitter', 'blogger'],
-      internet = [...video, ...blogs, 'vk', 'instagram'];
-
-console.log(internet);
-
-function log(a,b,c){
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const num = [2, 5, 8];
-
-log(...num);
-
-const ar = [4,3,0];
-const NewAr = [...ar];
-
-const aaa = {
-    first: 1,
-    second: 2
-};
-
-const newA = {...aaa};
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: "POST" ,
+    body: JSON.stringify({name: "Alex"}),
+    headers: {
+        'Content-type': 'application/json'
+    }
+})
+.then(response => response.json())
+.then(json => console.log(json));
